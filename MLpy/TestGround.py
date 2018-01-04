332171caf6b87e19025336d224bd3a124bc6b348
djimon <Djimon@gmx.de>
2018-01-04 13:20:38 +0100

    Initialized python-project for Programming task 4

---
+++ MLpy/TestGround.py
@@ -0,0 +1,6 @@
+#TODO: classify car data with a Naive Bayes classifier
+#TODO: use laplace smoothing
+#TODO: Split data: 2/3 train  <-> 1/3 test (crossvalidation)
+#TODO: Determine the mean error rate over 100 different random samples of training data
+#TODO: plot a confusion matrix for one sample
+
