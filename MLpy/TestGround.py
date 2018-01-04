14fff609d9d4ba6bfde49550676ff670f06d7ebe
djimon <Djimon@gmx.de>
2018-01-04 13:29:58 +0100

    little helper for starting (see TUTORIAL.txt)

--- MLpy/TestGround.py
+++ MLpy/TestGround.py
@@ -4,3 +4,4 @@
 #TODO: Determine the mean error rate over 100 different random samples of training data
 #TODO: plot a confusion matrix for one sample
 
+#inspiration: http://dataaspirant.com/2017/02/06/naive-bayes-classifier-machine-learning/
